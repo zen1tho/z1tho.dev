b3aec21f11441a790db802cf4e2449cebdf601a4
zen1tho <zen1tho.codes@gmail.com>
2023-01-30 22:35:22 +1100

    nuke project for sveltekit. Solid-Start is too unstable at this point.

--- src/routes/About.tsx
+++
@@ -1,7 +0,0 @@
-import { Component } from "solid-js";
-
-const About: Component = (props) => {
-  return <main>Hello</main>;
-};
-
-export default About;
--- src/routes/Project.tsx
+++
@@ -1,7 +0,0 @@
-import { Component } from "solid-js";
-
-const Project: Component<{}> = (props) => {
-  return <main>Project</main>;
-};
-
-export default Project;
