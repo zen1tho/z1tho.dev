0fb1be3aa783d9e244cc151d831a677f87e1a263
zen1tho <zen1tho.codes@gmail.com>
2023-02-03 12:45:58 +1100

    explicitly set port

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 5173 // Explicity set to 5173 because im used to it
+	}
 };
 
 export default config;
